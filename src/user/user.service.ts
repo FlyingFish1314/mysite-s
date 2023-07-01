e21e4507dd926386527c2ba86c61bc8714093a18
FlyingFish1314 <79733953+FlyingFish1314@users.noreply.github.com>
2023-07-01 15:53:59 +0800

    feat: 初步创建用户模块、以及添加热重载

---
+++ src/user/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  getUsers() {
+    return {
+      code: 0,
+      data: [],
+      msg: 'aaaa',
+    };
+  }
+}
