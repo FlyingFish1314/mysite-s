a62cda92296bef18116f9b8d2108b4e42527de5e
FlyingFish1314 <79733953+FlyingFish1314@users.noreply.github.com>
2023-07-01 17:23:21 +0800

    ci: 初始化全局配置，数据库信息，使用docker创建mysql数据库

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
-
+import { ConfigService } from '@nestjs/config/dist';
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   getUsers(): any {
+    const db = this.configService.get('DB');
+    const host = this.configService.get('DB_HOST');
+    console.log(
+      '🚀 ~ file: user.controller.ts:15 ~ UserController ~ getUsers ~ host:',
+      host,
+    );
+    console.log(
+      '🚀 ~ file: user.controller.ts:14 ~ UserController ~ getUsers ~ db:',
+      db,
+    );
     return this.userService.getUsers();
   }
 }
